f6701819683e43440dad670a7ffa5d5bb77c7f60
Mahadi Babiker <mahadib124@gmail.com>
2022-01-17 22:28:19 +0100

    Adding Appointments

---
+++ app/src/main/java/com/example/dentalapplicationproject/AppointmentsActivity.java
@@ -0,0 +1,14 @@
+package com.example.dentalapplicationproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AppointmentsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appointments);
+    }
+}
